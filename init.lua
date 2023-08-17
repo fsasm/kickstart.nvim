5980ef5d56a4239da4087aa3c0febfc6594448e9
Fabjan Sukalia <fsukalia@gmail.com>
2023-08-17 10:18:18 +0200

    autocompletion replaces the string when selected

--- init.lua
+++ init.lua
@@ -486,7 +486,10 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }
   },
   sources = {
     { name = 'nvim_lsp' },
