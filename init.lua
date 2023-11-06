2844e8139cf9031dfdd084ef25ea59452ac21870
Fabjan Sukalia <fsukalia@gmail.com>
2023-11-06 08:57:34 +0100

    add shortcut to format code via LSP

--- init.lua
+++ init.lua
@@ -441,6 +441,7 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+  nmap('<leader>fc', vim.lsp.buf.format, '[F]ormat [C]ode')
 end
 
 -- Enable the following language servers
